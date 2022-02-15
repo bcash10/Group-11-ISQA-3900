0bd81fb4deb25df7a486c766fc7e6f0462b9bb6e
bcash10 <80350534+bcash10@users.noreply.github.com>
2022-02-15 14:36:17 -0600

    Initial commit

---
+++ mailingsystem/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from .views import index
+
+
+urlpatterns = [
+    path('', index, name='index')
+]
+from django.urls import include, path
+from .views import index
+
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('getdata/', index, name='views.index'),
+]
