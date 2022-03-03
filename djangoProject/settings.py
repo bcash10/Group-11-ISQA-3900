4b7dbec9b2b7f040ba0ff3ec3686d4bd44e49b22
bcash10 <80350534+bcash10@users.noreply.github.com>
2022-03-03 14:14:33 -0600

    Created sample Css file for navbar, may have issues

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    ...
+    'django.core.context_processors.request',
+)
